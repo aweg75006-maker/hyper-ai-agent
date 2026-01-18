034271710209589f8c96a7da6c934dea8670656c
unknown <yzz>
2026-01-18 18:26:08 +0800

    ChatPDF 后端

--- src/main/java/com/yzz/hyperaiagent/config/CommonConfiguration.java
+++ src/main/java/com/yzz/hyperaiagent/config/CommonConfiguration.java
@@ -4,10 +4,12 @@ import com.yzz.hyperaiagent.advisor.MyLoggerAdvisor;
 import com.yzz.hyperaiagent.chatmemory.FileBasedChatMemory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
@@ -18,11 +20,17 @@ public class CommonConfiguration {
 
     // 之后可以存到MySQL里面
     String fileDir = System.getProperty("user.dir") + "/tmp/chat-memory2";
+    String fileDir2 = System.getProperty("user.dir") + "/tmp/chat-memory-pdf";
     ChatMemory chatMemory = new FileBasedChatMemory(fileDir);
+    ChatMemory chatMemoryPdf = new FileBasedChatMemory(fileDir2);
 
     MessageChatMemoryAdvisor memoryAdvisor =
             MessageChatMemoryAdvisor.builder(chatMemory)
                     .build();
+    MessageChatMemoryAdvisor memoryPdfAdvisor =
+            MessageChatMemoryAdvisor.builder(chatMemoryPdf)
+                    .build();
+
 
     @Bean
     public VectorStore vectorStore(EmbeddingModel dashscopeEmbeddingModel) {
@@ -38,6 +46,26 @@ public class CommonConfiguration {
                 .defaultAdvisors(
                         new MyLoggerAdvisor(),
                         memoryAdvisor
+                )
+                .build();
+    }
+
+    @Bean
+    public ChatClient pdfChatClient(ChatModel chatModel, VectorStore vectorStore) {
+        return ChatClient
+                .builder(chatModel)
+                .defaultOptions(ChatOptions.builder().model("qwen-flash").build())
+                .defaultSystem("请根据上下文回答问题，遇到上下文没有的问题，不要随意编造。")
+                .defaultAdvisors(
+                        new MyLoggerAdvisor(),
+                        memoryPdfAdvisor,
+                        new QuestionAnswerAdvisor(
+                                vectorStore,
+                                SearchRequest.builder()
+                                        .similarityThreshold(0.6)
+                                        .topK(2)
+                                        .build()
+                        )
                 )
                 .build();
     }
--- src/main/java/com/yzz/hyperaiagent/rag/PsyAppDocumentLoader.java
+++ src/main/java/com/yzz/hyperaiagent/rag/PsyAppDocumentLoader.java
@@ -2,8 +2,12 @@ package com.yzz.hyperaiagent.rag;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.JsonReader;
 import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
 import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Component;
@@ -42,6 +46,29 @@ public class PsyAppDocumentLoader {
             }
         } catch (IOException e) {
             log.error("Markdown 文档加载失败", e);
+        }
+        return allDocuments;
+    }
+
+    public List<Document> loadPdfs() {
+        List<Document> allDocuments = new ArrayList<>();
+        // 加载多篇PDF文件
+        try {
+            Resource[] resources = resourcePatternResolver.getResources("classpath:document2/*.pdf");
+            for (Resource resource : resources) {
+                PdfDocumentReaderConfig config = PdfDocumentReaderConfig.builder()
+                        .withPageTopMargin(0)
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
+                                .withNumberOfTopTextLinesToDelete(0)
+                                .withNumberOfBottomTextLinesToDelete(0)
+                                .build())
+                        .withPagesPerDocument(1)
+                        .build();
+                PagePdfDocumentReader pdfDocumentReader = new PagePdfDocumentReader(resource, config);
+                allDocuments.addAll(pdfDocumentReader.get());
+            }
+        } catch (IOException e) {
+            log.error("PDF 文档加载失败", e);
         }
         return allDocuments;
     }
